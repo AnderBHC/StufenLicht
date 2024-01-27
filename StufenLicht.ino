fd7f255815d08b8cb2bbeb4798d48d87ad0be494
Anders <43926857+AnderBHC@users.noreply.github.com>
2024-01-27 20:32:50 +0100

    preferenes

--- StufenLicht.ino
+++ StufenLicht.ino
@@ -1,19 +1,19 @@
-#include <NeoPixelBus.h>
-#include <esp_dmx.h>
 #include <Arduino.h>
+#include <esp_dmx.h>
+#include <NeoPixelBus.h>
+#include <Preferences.h>
 
-#define DMXStart 1
-#define PinStripA 27
-#define NumPixelA 100
-#define PinStripB 26
-#define NumPixelB 100
+Preferences preferences;
 
-#define transmitPin 17
-#define recievePin 16
-#define enablePin 21
-dmx_port_t dmxPort = 2;
+int receivePin = 3;
+int transmitPin = 1;
+int enablePin = 4;
+
+dmx_port_t dmxPort = 1;
 byte data[DMX_MAX_PACKET_SIZE];
 QueueHandle_t queue;
+unsigned int timer = 0;
+bool dmxIsConnected = false;
 
 uint8_t Red = 0;
 uint8_t Blue = 0;
@@ -21,16 +21,36 @@ uint8_t Green = 0;
 uint8_t Mode = 0;
 uint8_t Speed = 0;
 uint32_t lastShow = 0;
-byte blikstate = 0;
+
+int32_t lastBlink = 0;
+byte blinkState = 0;
 uint8_t progress = 0;
 
-NeoPixelBus<NeoGrbFeature, NeoEsp32Rmt0800KbpsMethod> StripA (NumPixelA, PinStripA);
-NeoPixelBus<NeoGrbFeature, NeoEsp32Rmt0800KbpsMethod> StripB (NumPixelB, PinStripB);
+NeoPixelBus<NeoGrbFeature, NeoEsp32Rmt0Sk6812Method> StripA (NumPixelA, PinStripA);
+NeoPixelBus<NeoGrbFeature, NeoEsp32Rmt1Sk6812Method> StripB (NumPixelB, PinStripB);
+
 
 void setup(){
+  Serial.begin(115200);
+  preferences.begin("DMXPreferences",RO_MODE);
+  if !isKey("DMXStartAddr")
+    preferences.begin("DMXPreferences", RW_MODE);
+    //first boot enter Web config
+  }
+  else{
+    DMXStart = preferences.getUShort("DMXStartAddr");
+    NumPixelA = preferences.getUInt("NumPixelStipA");
+    NumPixelB = preferences.getUInt("NumPixelStipB");
+    NumPixelC = preferences.getUInt("NumPixelStripC");
+    NumPixelD = preferences.getUInt("NumPixelStripD");
+  }
+
   dmx_config_t dmxConfig = DMX_DEFAULT_CONFIG;
   dmx_param_config(dmxPort, &dmxConfig);
   dmx_set_pin(dmxPort, transmitPin, recievePin, enablePin);
+  int queueSize = 1;
+  int interruptPriority = 1;
+  dmx_driver_install(dmxPort, DMX_MAX_PACKET_SIZE, queueSize, &queue, interruptPriority);
 
   StripA.Begin();
   StripB.Begin();
@@ -43,26 +63,41 @@ void loop(){
   dmx_event_t packet;
   if(xQueueReceive(queue, &packet, DMX_RX_PACKET_TOUT_TICK)){
     if(packet.status == DMX_OK){
+      if (!dmxIsConnected){
+        Serial.println("DMX connected!");
+        dmxIsConnected = true;
+      }
       dmx_read_packet(dmxPort, data, packet.size);
+      timer += packet.duration;
+
       Red = data[DMXStart];
       Green = data[DMXStart + 1];
       Blue = data[DMXStart + 2];
       Mode = data[DMXStart + 3];
       Speed = data[DMXStart +4];
+
+      if (timer >= 1000000){
+        Serial.printf("Start code is 0x%02X and slot 4 is 0x%02X\n", data[0], data[4]);
+        timer -= 1000000;
+      }
+    }
+    else{
+      Serial.println("DMX error!");
     }
   }
-  else{
+  else if(dmxIsConnected){
     Red = 0;
     Green = 0;
     Blue = 0;
     Mode = 0;
     Speed = 0;
+    Serial.println("DMX timed out!");
   }
   uint8_t* PixelsA = StripA.Pixels();
   uint8_t* PixelsB = StripB.Pixels();
 
   //Static
-  if (Mode >= 0 && Mode < 63){
+  if (Mode < 64){
       for (int i = 0; i < NumPixelA; i++){
         PixelsA[i * 3] = Red;
         PixelsA[i * 3 + 1] = Green;
@@ -77,23 +112,32 @@ void loop(){
       StripB.Dirty();
   }
   //Blinken
-//   else if (Mode >= 1 && Mode <2){
-//     if (millis() - lastTime > (255-Speed)*4){
-//       lastTime = millis();
-//       if (blinkstate == 0){
-//           blinkstate = 1;
-//       }
-//       else{
-//         blinkstate = 0;
-//       }
-//     }
-//     for (i = 0; i < NumPixelA; i++){
-//       StripA.setPixel(i,Red*blinkstate,Blue*blinkstate,Green*blinkstate);
-//     }
-//     for (i = 0; i <NumPixelB; i++){
-//       StripB.setPixel(i,Red*blinkstate,Blue*blinkstate,Green*blinkstate);
-//     }
-//   }
+  else if (Mode >= 64 && Mode < 128){
+    int calc = -6 * Speed + 1750;
+    if (blinkState == 0 && millis() - lastBlink > 500){
+      blinkState == 1;
+      lastBlink = millis();
+
+    }
+    else if (blinkState == 1 && millis() - lastBlink > 250){
+      blinkState = 0;
+      lastBlink = millis();
+
+    }
+
+    for (int i = 0; i < NumPixelA; i++){
+      PixelsA[i * 3] = Red * blinkState;
+      PixelsA[i * 3 + 1] = Green * blinkState;
+      PixelsA[i * 3 + 2] = Blue * blinkState;
+    }
+    for (int i = 0; i < NumPixelB; i++){
+      PixelsB[i * 3] = Red * blinkState;
+      PixelsB[i * 3 + 1] = Green * blinkState;
+      PixelsB[i * 3 + 2] = Blue * blinkState;
+    }
+    StripA.Dirty();
+    StripB.Dirty();
+  }
 //   //Rainbow
 //   else if (Mode >=2 && Mode < 3){
 //     uint32_t offset = millis() * Speed >> 10;
@@ -106,7 +150,7 @@ void loop(){
 //   }
 //   //Pulse
 //   else if (Mode>= 3 && Mode<4){
-//     if millis()-lastTime > (256-Speed){
+//     if millis()-lastBlink > (256-Speed){
 //       progress++;
 //     }
 //     for ( int i = 0; i++; i < NumPixelA){
@@ -128,47 +172,44 @@ void loop(){
 //       }
 //     }
 //   }
-  if (millis() - lastShow > 42){
-    lastShow = millis();
-    StripA.Show();
-    StripB.Show();
-  }
+  StripA.Show();
+  StripB.Show();
   }
 
-byte RainbowRot(unsigned int offset){
-  int c = offset%766;
-  if (c >= 0 && c <= 255){
-    return 255 - c;
-  }
-  if (c > 255 && c <= 510) {
-    return 0;
-  }
-  if (c > 510){
-    return c - 510;
-  }
-}
-byte RainbowGruen(unsigned int offset){
-  int c = offset%766;
-  byte color;
-  if (c >= 0 && c <= 255){
-    return c;
-  }
-  if (c > 255 && c <= 510){
-    return 510 - c;
-  }
-  if(c > 510){
-    return 0;
-  }
-}
-byte RainbowBlau(unsigned int offset){
-  int c = offset%766;
-  if(c >= 0 && c <= 255){
-    return 0;
-  }
-  if (c>=256 && c<510){
-    return c - 255;
-  }
-  if (c >= 510){
-    return 765 - c;
-  }
-}
+// byte RainbowRot(unsigned int offset){
+//   int c = offset%766;
+//   if (c >= 0 && c <= 255){
+//     return 255 - c;
+//   }
+//   if (c > 255 && c <= 510) {
+//     return 0;
+//   }
+//   if (c > 510){
+//     return c - 510;
+//   }
+// }
+// byte RainbowGruen(unsigned int offset){
+//   int c = offset%766;
+//   byte color;
+//   if (c >= 0 && c <= 255){
+//     return c;
+//   }
+//   if (c > 255 && c <= 510){
+//     return 510 - c;
+//   }
+//   if(c > 510){
+//     return 0;
+//   }
+// }
+// byte RainbowBlau(unsigned int offset){
+//   int c = offset%766;
+//   if(c >= 0 && c <= 255){
+//     return 0;
+//   }
+//   if (c>=256 && c<510){
+//     return c - 255;
+//   }
+//   if (c >= 510){
+//     return 765 - c;
+//   }
+// }
